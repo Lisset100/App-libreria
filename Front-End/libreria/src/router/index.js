fe8b908d8602b1a136e2991072df418edaed2cb2
Lisset100 <liset10nieve@live.com.mx>
2023-09-13 13:03:09 -0600

    fix: se agregaron librerias faltantes

--- Front-End/libreria/src/router/index.js
+++ Front-End/libreria/src/router/index.js
@@ -1,22 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
-import Books from '../views/Books.vue';
-import Ebooks from '../views/Ebooks.vue';
-import Categories from '../views/Categories.vue';
-import Catatalog from '../views/Catalog.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Books from "../views/Books.vue";
+import Ebooks from "../views/Ebooks.vue";
+import Catatalog from "../views/Catalog.vue";
 const routes = [
-  { path: '/', component: Home },
-  { path: '/home', component: Home },
-  { path: '/books', component: Books },
-  { path: '/ebooks', component: Ebooks},
-  { path: '/catalog', component: Catatalog},
-{ path: '/categories', component: Categories}
-
+  { path: "/", component: Home },
+  { path: "/home", component: Home },
+  { path: "/books", component: Books },
+  { path: "/ebooks", component: Ebooks },
+  { path: "/catalog", component: Catatalog },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 });
 
 export default router;
